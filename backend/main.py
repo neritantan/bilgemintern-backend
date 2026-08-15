ddc754a502318beef1c1f2cb9d45220d249d4160
neritantan <enesibrahim58@gmail.com>
2026-08-16 01:35:37 +0300

    added endpoint templates

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import random
 
-
+# db baglanacak note endpointleri ile dbden veri cekilecek basit bir frontend vibe-coded
 app = FastAPI()
+
+class Note(BaseModel):
+    note_id: int
+    note: str
+
+
+
 
 @app.get("/")
 async def root():
-    return{"message": "basarili"}
+    fruits = ("apple", "banana", "strawberry", "mango")
+    return {"fruit": random.choice(fruits)}
+
+@app.get("/notes")
+async def get_notes():
+    #once dbdeki son notu sonra tum notlari return edecek
+    return None
+
+@app.post("/notes")
+async def post_note():
+    #db e son notun sonuna +1 ekleyip id olarak gonderecek aciklamayla
+    return None
+
+
+
+@app.get("/notes/{note_id}")
+async def get_note(note_id: int):
+    return {"note_id": note_id,
+            "desc": "blablalbalba"}
+
