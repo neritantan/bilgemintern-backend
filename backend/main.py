e7934f0af1b88f0cb5de7a93db149f5c0b952de3
neritantan <enesibrahim58@gmail.com>
2026-08-16 16:04:56 +0300

    updated endpoint templates

--- backend/main.py
+++ backend/main.py
@@ -5,32 +5,40 @@ import random
 # db baglanacak note endpointleri ile dbden veri cekilecek basit bir frontend vibe-coded
 app = FastAPI()
 
-class Note(BaseModel):
+class NoteUser(BaseModel):
+    text: str
+
+class NoteDB(BaseModel):
     note_id: int
-    note: str
+    text: str
 
-
-
+fruits = ["apple", "banana", "strawberry", "mango"]
+notes = []
+notecount = len(notes)
 
 @app.get("/")
 async def root():
-    fruits = ("apple", "banana", "strawberry", "mango")
     return {"fruit": random.choice(fruits)}
 
 @app.get("/notes")
 async def get_notes():
     #once dbdeki son notu sonra tum notlari return edecek
-    return None
+    return notes
 
-@app.post("/notes")
-async def post_note():
+@app.post("/notes") #POST
+async def post_note(note: NoteUser):
+    global notecount
     #db e son notun sonuna +1 ekleyip id olarak gonderecek aciklamayla
-    return None
-
-
+    noteToDB = {"note_id": len(notes),
+                "text": note.text }
+    
+    notes.append(noteToDB)
+    notecount = len(notes) # bunun global olmasi gerek
+    return notes
 
 @app.get("/notes/{note_id}")
 async def get_note(note_id: int):
     return {"note_id": note_id,
-            "desc": "blablalbalba"}
+            "text": "blablalbalba",
+            "fruit": fruits[note_id]}
 
